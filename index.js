4e0c5f36e109a482062a3c6a0138e50e4d866373
saransh <sethia.saransh.ss@gmail.com>
2024-05-15 18:31:34 +0530

    second commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config({ path: "src/.env" });
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const authRoutes = require("./routes/authRoutes");
-const taskRoutes = require("./routes/taskRoutes");
+const authRoutes = require("./src/routes/authRoutes");
+const taskRoutes = require("./src/routes/taskRoutes");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
@@ -18,7 +18,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
-app.get("/", (req, res) => {
+app.get("/api/tasks", (req, res) => {
   res.send("Backend server is running");
 });
 
